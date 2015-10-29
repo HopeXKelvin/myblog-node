37085e9d42d70cd97ca37aea0dca65613e599665
kelvin <hope_kelvin@163.com>
2015-10-29 13:02:50 +0800

    routes 的index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fs=require("fs");
 
 /* GET home page. */
-/*router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});*/
-
-router.get('/',function(req,res){
-	fs.readFile('./blogs/test.md','utf-8',function(err,data){
-		if(err){
-			res.send(err);
-		}
-		res.send(data);
-	});
 });
 
 module.exports = router;
